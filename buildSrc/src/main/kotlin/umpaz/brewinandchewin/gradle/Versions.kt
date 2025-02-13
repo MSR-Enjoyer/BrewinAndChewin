f93bf14f795dbf037b1d2438efa50efab2579e83
MerchantPug <merchantpug@gmail.com>
2025-02-13 17:55:19 +1100

    Update versions.

--- buildSrc/src/main/kotlin/umpaz/brewinandchewin/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/umpaz/brewinandchewin/gradle/Versions.kt
@@ -1,7 +1,7 @@
 package umpaz.brewinandchewin.gradle
 
 object Versions {
-    const val MOD = "4.0.0-beta.6"
+    const val MOD = "4.0.0-beta.7"
 
     const val MINECRAFT = "1.21.1"
     const val PARCHMENT_MINECRAFT = "1.21"
