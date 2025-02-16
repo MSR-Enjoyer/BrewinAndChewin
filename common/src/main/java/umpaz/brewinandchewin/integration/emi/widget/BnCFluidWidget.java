1ae82743c5066a7a528d76ec6e951b8ede0bb35e
MerchantPug <merchantpug@gmail.com>
2025-02-16 13:13:19 +1100

    Fix fluid displays not being animated in BnCFluidWidget.

--- common/src/main/java/umpaz/brewinandchewin/integration/emi/widget/BnCFluidWidget.java
+++ common/src/main/java/umpaz/brewinandchewin/integration/emi/widget/BnCFluidWidget.java
@@ -29,25 +29,22 @@ public class BnCFluidWidget extends SlotWidget {
         output = true;
     }
 
-    public void drawBackground(GuiGraphics gui, int mouseX, int mouseY, float delta) {
-        if (!BnCConfiguration.CLIENT_CONFIG.get().renderFluidInKeg())
-            return;
-        Bounds bounds = this.getBounds();
-        BrewinAndChewinClient.getHelper().renderFluidInKeg(fluidStack, gui, bounds.x() + 2, bounds.y() + 2, 1.0F);
-    }
+    public void drawBackground(GuiGraphics gui, int mouseX, int mouseY, float delta) {}
 
     @Override
-    public void drawStack(GuiGraphics draw, int mouseX, int mouseY, float delta) {
+    public void drawStack(GuiGraphics gui, int mouseX, int mouseY, float delta) {
         Bounds bounds = this.getBounds();
+        if (BnCConfiguration.CLIENT_CONFIG.get().renderFluidInKeg())
+            BrewinAndChewinClient.getHelper().renderFluidInKeg(fluidStack, gui, bounds.x() + 2, bounds.y() + 2, 1.0F);
         int xOff = (bounds.width() - 16) / 2;
         int yOff = (bounds.height() - 16) / 2 - 4;
-        item.render(draw, bounds.x() + xOff, bounds.y() + yOff, delta);
+        item.render(gui, bounds.x() + xOff, bounds.y() + yOff, delta);
     }
 
     @Override
-    public void drawOverlay(GuiGraphics draw, int mouseX, int mouseY, float delta) {
+    public void drawOverlay(GuiGraphics gui, int mouseX, int mouseY, float delta) {
         Bounds bounds = this.getBounds();
-        draw.blit(FermentingEmiRecipe.BACKGROUND, bounds.x() + 1, bounds.y() + 1, 170, 45, bounds.width(), bounds.height() - 2);
-        super.drawOverlay(draw, mouseX, mouseY, delta);
+        gui.blit(FermentingEmiRecipe.BACKGROUND, bounds.x() + 1, bounds.y() + 1, 170, 45, bounds.width(), bounds.height() - 2);
+        super.drawOverlay(gui, mouseX, mouseY, delta);
     }
 }
