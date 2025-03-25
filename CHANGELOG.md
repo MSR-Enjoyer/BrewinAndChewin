d367e39b0a627b95f3f4525aedf76a4bece7a63a
MerchantPug <merchantpug@gmail.com>
2025-03-25 11:47:35 +1100

    Fix chat scrambling on NeoForge.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,2 @@
 ## Bugfixes
-- Fixed Tipsy chat scrambling compatibility with any mods that modify chat messages to have different colors (For example, [Styled Chat](https://modrinth.com/mod/styled-chat/))
-- [FABRIC] Fixed no Tipsified chat messages being cancelled on the client.
+- [NEOFORGE] Fixed chat scrambling not functioning on NeoForge.
--- neoforge/src/main/java/umpaz/brewinandchewin/neoforge/client/TipsyEffects.java
+++ neoforge/src/main/java/umpaz/brewinandchewin/neoforge/client/TipsyEffects.java
@@ -1,7 +1,12 @@
 package umpaz.brewinandchewin.neoforge.client;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.PlayerChatMessage;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModList;
@@ -12,22 +17,35 @@ import umpaz.brewinandchewin.BrewinAndChewin;
 import umpaz.brewinandchewin.client.utility.BnCClientTextUtils;
 import umpaz.brewinandchewin.neoforge.client.integration.IntoxicationAppleSkinCompatNeoForge;
 
+import java.util.List;
+import java.util.Optional;
+
 @EventBusSubscriber(modid = BrewinAndChewin.MODID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
 public class TipsyEffects {
     @SubscribeEvent
     public static void whatsYourName(RenderNameTagEvent event) {
-        Component newName = BnCClientTextUtils.nameTagRenderer(event.getContent());
-        if (event.getContent() != newName)
-            event.setContent(newName);
+//        Component newName = BnCClientTextUtils.nameTagRenderer(event.getContent());
+//        if (event.getContent() != newName)
+//            event.setContent(newName);
     }
 
     @SubscribeEvent
     public static void iCanHear(ClientChatReceivedEvent.Player event) {
-        BnCClientTextUtils.setupChatMessage(event.getPlayerChatMessage().withUnsignedContent(event.getMessage()));
-        PlayerChatMessage chatMessage = BnCClientTextUtils.getTipsyMessage();
-        if (chatMessage != null && chatMessage.filterMask().isEmpty() && !chatMessage.decoratedContent().equals(event.getPlayerChatMessage().decoratedContent()) && event.getBoundChatType() != null)
-            event.setMessage(event.getBoundChatType().decorate(chatMessage.decoratedContent()));
+        BnCClientTextUtils.setupChatMessage(event.getPlayerChatMessage().withUnsignedContent(getChatMessage(event.getMessage())));
+        PlayerChatMessage tipsyMessage = BnCClientTextUtils.getTipsyMessage();
+        if (tipsyMessage != null && event.getBoundChatType() != null) {
+            ChatType.Bound newBound = new ChatType.Bound(Minecraft.getInstance().level.registryAccess().lookupOrThrow(Registries.CHAT_TYPE).getOrThrow(ChatType.CHAT), getPlayerNameComponent(event.getBoundChatType().name()), Optional.empty());
+            event.setMessage(newBound.decorate(tipsyMessage.decoratedContent()));
+        }
         BnCClientTextUtils.clearTipsyMessage();
+    }
+
+    private static Component getPlayerNameComponent(Component component) {
+        return component.getSiblings().getFirst();
+    }
+
+    private static Component getChatMessage(Component component) {
+        return (Component)((TranslatableContents)component.getContents()).getArgs()[1];
     }
 
     static {
